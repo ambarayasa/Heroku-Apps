59bfacd3564f7ec93e98ad838301a0bdcf27d5f4
Ambara Yasa <81506775+ambarayasa@users.noreply.github.com>
2021-04-01 14:28:38 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php.sample
+$config = [
+  'settings' => [
+    'displayErrorDetails' => true,
+    'db' => [
+      'hostname' => 'localhost',
+      'username' => 'root',
+      'password' => '',
+      'name' => 'db_testims',   
+    ]
+  ]
+];
