1deb90d632ad235922d1a53e37aeeac0e6bdad01
Ambara Yasa <81506775+ambarayasa@users.noreply.github.com>
2021-04-01 14:30:32 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $config = [
   'settings' => [
     'displayErrorDetails' => true,
     'db' => [
-      'hostname' => 'localhost',
-      'username' => 'root',
-      'password' => '',
-      'name' => 'db_testims',   
+      'hostname' => 'ec2-54-90-13-87.compute-1.amazonaws.com',
+      'username' => 'ovodzilbmjjbmf',
+      'password' => 'b68fe43f0000e04fe164ecc6993df11dd31238d376dd6af4a9c84b6c61481c34',
+      'name' => 'dansnkjs1t8tca',   
     ]
   ]
 ];
